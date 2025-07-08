45fb8a97e5c2d0cba9667559a9109ca38780649e
Banti Gheneti <banti@gheneti.com>
2025-07-07 23:46:00 -0400

    trigger new build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
+// https://astro.build/config 
 export default defineConfig({
   integrations: [mdx()],
   site: 'https://bgheneti.github.io',
